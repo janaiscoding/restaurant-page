bbf092f87bf33e85ab367620ee0025dfb3a68951
JanaIsCoding <jana.istrate@gmail.com>
2023-01-29 19:18:24 +0200

    Add JS notes and CSS reset sheet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 console.log("This is a test.");
+
+// here I will add event listeners which will clear the entire content
+// of the webpage, and replace it with the current clicked module
+// for each tab: Home / Menu / Contact
---
+++ src/modules/menu.js
@@ -0,0 +1,3 @@
+//module will export a function that creates a div element
+// adds the appropriate content and styles to that element 
+//and then appends it to the DOM
