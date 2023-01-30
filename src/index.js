8254dff30856b121955aee7d3509ba40267e4f16
JanaIsCoding <jana.istrate@gmail.com>
2023-01-30 13:55:33 +0200

    Remove webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import './style.css';
-function test(){
+
 console.log("This is a test.");
-}
-test();
 
 // here I will add event listeners which will clear the entire content
 // of the webpage, and replace it with the current clicked module
 // for each tab: Home / Menu / Contact
-
-function component() {
-    const element = document.createElement('div');
-    element.innerText = "This is a test."
-    element.classList.add('test');
-    return element;
-} 
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
   mode: "development",
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
   module: {
     rules: [
